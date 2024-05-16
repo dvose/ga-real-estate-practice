0c45e05ceb9a0be769470fa35c66da52fb2937c9
Dustin Vose <dustin@fivebox.com>
2024-05-16 15:05:43 -0400

    Add Unit 3

--- src/Constants.ts
+++ src/Constants.ts
@@ -533,7 +533,271 @@ const UNIT_2: Question[] = [
   }
 ];
 
+const UNIT_3: Question[] = [
+  {
+    key: "u3_1",
+    body: "The right of a government body to take ownership of real estate for public use is called",
+    answers: [
+      "escheat.",
+      "emment domain.",
+      "condemnation.",
+      "police power."
+    ],
+    correctAnswer: "emment domain.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_2",
+    body: "One who has ownership rights of real estate that could continue forever and which provide that no other person can claim to be the owner of or have any ownership control over the property has",
+    answers: [
+      "fee simple absolute.",
+      "life estate.",
+      "determinable fee.",
+      "condition subsequent."
+    ],
+    correctAnswer: "fee simple absolute.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_3",
+    body: "The owner of fee simple title to a vacant lot adjacent to a hospital decided to make a gift of the lot to the hospital. The deed conveyed ownership of the lot to the hospital 'so long as it is used for hospital purposes.' After completion of the gift, the hospital will own",
+    answers: [
+      "a fee simple absolute estate.",
+      "a license.",
+      "a fee simple determinable.",
+      "a leasehold estate."
+    ],
+    correctAnswer: "a fee simple determinable.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_4",
+    body: "Your neighbors use your driveway to reach the garage on their property. Your attorney explains that the neighbors' real estate includes an easement appurtenant giving them the right to do this. Your property is",
+    answers: [
+      "the leasehold interest.",
+      "the dominant tenement.",
+      "servient tenement.",
+      "the license property."
+    ],
+    correctAnswer: "servient tenement.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_5",
+    body: "A license is an example of",
+    answers: [
+      "an easement appurtenant.",
+      "an encroachment.",
+      "a personal privilege.",
+      "a restriction."
+    ],
+    correctAnswer: "a personal privilege.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_6",
+    body: "An easement appurtenant",
+    answers: [
+      "terminates with the sale of the property.",
+      "is a right-of-way for a utility company.",
+      "is revocable.",
+      "runs with the land."
+    ],
+    correctAnswer: "runs with the land.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_7",
+    body: "A property on Main Street that was formerly a retail store will become the site of a new city hall, made possible by the government's power of",
+    answers: [
+      "escheat.",
+      "possibility of reverter.",
+      "eminent domain.",
+      "taxation."
+    ],
+    correctAnswer: "eminent domain.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_8",
+    body: "Which of the following is NOT an example of governmental power?",
+    answers: [
+      "Remainder",
+      "Police power",
+      "Eminent domain",
+      "Taxation"
+    ],
+    correctAnswer: "Remainder",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_9",
+    body: "A property owner who has the legal right to use a neighbor's land holds",
+    answers: [
+      "an estate in land.",
+      "an easement.",
+      "a police power.",
+      "an encroachment."
+    ],
+    correctAnswer: "an easement.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_10",
+    body: "Which of the following is a legal life estate?",
+    answers: [
+      "Leasehold",
+      "Fee simple absolute",
+      "Homestead",
+      "Determinable fee"
+    ],
+    correctAnswer: "Homestead",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_11",
+    body: "An owner conveys ownership of a residence to a church but reserves a life estate in the residence. The future interest held by the church is",
+    answers: [
+      "a pur autre vie.",
+      "a reminder",
+      "a reversion.",
+      "a leasehold."
+    ],
+    correctAnswer: "a reminder",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_12",
+    body: "The new owner of a property installs a fence on the property. By mistake, the fence extends one foot over the lot line onto a neighbor's property. The fence is an example of",
+    answers: [
+      "a license.",
+      "an encroachment.",
+      "an easement by necessity.",
+      "an easement by prescription."
+    ],
+    correctAnswer: "an encroachment.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_13",
+    body: "A homeowner may be allowed certain protection from judgments of creditors as a result of the state's",
+    answers: [
+      "littoral rights.",
+      "curtesy rights.",
+      "homestead rights.",
+      "dower rights."
+    ],
+    correctAnswer: "homestead rights.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_14",
+    body: "A person has permission from a property owner to hike on the owner's property during the autumn months. The hiker has",
+    answers: [
+      "an easement by necessity.",
+      "an easement by condemnation.",
+      "riparian rights.",
+      "a license."
+    ],
+    correctAnswer: "a license.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_15",
+    body: "A homestead is a legal life estate in real estate that is",
+    answers: [
+      "leased by renters.",
+      "occupied as the family home.",
+      "used as a vacation home.",
+      "a secondary residence."
+    ],
+    correctAnswer: "occupied as the family home.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_16",
+    body: "Because a homeowner failed to pay the real estate taxes on time, the taxing authority imposed a claim against the homeowner's property. This claim is known as",
+    answers: [
+      "a deed restriction.",
+      "a lien.",
+      "an easement.",
+      "a reversionary interest."
+    ],
+    correctAnswer: "a lien.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_17",
+    body: "The type of easement that is a right-of-way for a utility company's power lines is",
+    answers: [
+      "an easement in gross.",
+      "an easement by necessity.",
+      "an easement by prescription.",
+      "a nonassignable easement."
+    ],
+    correctAnswer: "an easement in gross.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_18",
+    body: "The process by which government takes control of a property after the owner dies without a will or lawful heirs is",
+    answers: [
+      "escheat.",
+      "lis pendens.",
+      "condemnation.",
+      "taxation."
+    ],
+    correctAnswer: "escheat.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_19",
+    body: "A landowner has divided a large parcel of land into smaller parcels and has recently sold a tract near a nature preserve that is landlocked and cannot be entered except through one of the other tracts. The buyer of that property will probably be granted what type of easement by court action?",
+    answers: [
+      "Easement by necessity.",
+      "Easement in gross.",
+      "Easement by prescription.",
+      "Easement by condemnation."
+    ],
+    correctAnswer: "Easement by necessity.",
+    math: false,
+    unit: 3
+  },
+  {
+    key: "u3_20",
+    body: "All of the following will terminate an easement EXCEPT",
+    answers: [
+      "when the need no longer exists.",
+      "nonuse of a prescriptive easement.",
+      "abandonment of easement.",
+      "release of the right of easement to the dominant tenement."
+    ],
+    correctAnswer: "release of the right of easement to the dominant tenement.",
+    math: false,
+    unit: 3
+  }
+];
+
 export const QUESTIONS: Question[] = [
   ...UNIT_1,
-  ...UNIT_2
+  ...UNIT_2,
+  ...UNIT_3
 ];
