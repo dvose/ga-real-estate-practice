e7ff7f3a483274efcb032a17ab3de2b1cb205e0f
Dustin Vose <dustin@fivebox.com>
2024-05-16 16:47:59 -0400

    Add Unit 6

--- src/Constants.ts
+++ src/Constants.ts
@@ -1330,10 +1330,274 @@ const UNIT_5: Question[] = [
 
 ];
 
+const UNIT_6: Question[] = [
+  {
+    key: "u6_1",
+    body: "The basic requirements for a valid conveyance are governed by",
+    answers: [
+      "state law.",
+      "local custom.",
+      "national law.",
+      "the law of descent."
+    ],
+    correctAnswer: "state law.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_2",
+    body: "Every deed must be signed by",
+    answers: [
+      "the grantor.",
+      "the grantee.",
+      "the grantor and grantee.",
+      "the devisee."
+    ],
+    correctAnswer: "the grantor.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_3",
+    body: "A 15-year-old recently inherited a parcel of real estate and has decided to sell it. If the 15-year-old executes a deed conveying the property to a purchaser, such a conveyance would be",
+    answers: [
+      "valid.",
+      "void.",
+      "invalid.",
+      "voidable."
+    ],
+    correctAnswer: "voidable.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_4",
+    body: "A form authorizing one person to execute documents for another is called",
+    answers: [
+      "a power of attorney.",
+      "a release deed.",
+      "a quitclaim deed.",
+      "a power to represent."
+    ],
+    correctAnswer: "a power of attorney.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_5",
+    body: "The grantee receives greatest protection with what type of deed?",
+    answers: [
+      "Quitclaim",
+      "General warranty",
+      "Bargain and sale with covenant",
+      "Executor's"
+    ],
+    correctAnswer: "General warranty",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_6",
+    body: "Party Y receives a deed from party X. The granting clause of the deed states, 'I hereby remise, release, alienate, and convey to Y the following real property.' What type of deed has Y received?",
+    answers: [
+      "Special warranty",
+      "Quitclaim",
+      "General warranty",
+      "Bargain and sale"
+    ],
+    correctAnswer: "Special warranty",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_7",
+    body: "Under the covenant of quiet enjoyment, a grantor",
+    answers: [
+      "promises to obtain and deliver any instrument needed to make the title good.",
+      "warrants that the property is free from liens and encumbrances.",
+      "warrants that he or she is the owner and has the right to convey title to the property.",
+      "ensures that the title will be good against the title claims of third parties."
+    ],
+    correctAnswer: "ensures that the title will be good against the title claims of third parties.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_8",
+    body: "Which type of deed merely implies but does NOT specifically warrant that the grantor holds good title to the property?",
+    answers: [
+      "Special warranty",
+      "Bargain and sale",
+      "Quitclaim",
+      "Trust deed"
+    ],
+    correctAnswer: "Bargain and sale",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_9",
+    body: "At which of the following steps did title to the house actually transfer or pass to B?\nStep 1: A decided to convey a house to B.\nStep 2: A signed a deed transferring title to B.\nStep 3: A gave the signed deed to B, who accepted it.\nStep 4: B took the deed to the county recorder's office and had it recorded.",
+    answers: [
+      "Step 1",
+      "Step 2",
+      "Step 3",
+      "Step 4"
+    ],
+    correctAnswer: "Step 3",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_10",
+    body: "P signed a deed transferring ownership of P's house to Q. To provide evidence that P's signature was genuine, P executed a declaration before a notary. This declaration is known as",
+    answers: [
+      "an affidavit.",
+      "an acknowledgment.",
+      "an affirmation.",
+      "an estoppel."
+    ],
+    correctAnswer: "an acknowledgment.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_11",
+    body: "Title to real estate may be transferred during a person's lifetime by",
+    answers: [
+      "devise.",
+      "descent.",
+      "involuntary alienation.",
+      "escheat."
+    ],
+    correctAnswer: "involuntary alienation.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_12",
+    body: "A resident of Denver bought acreage in a distant county, never went to see the acreage, and did not use the ground. An artist moved a mobile home onto the land, had a water well drilled, and lived there for 22 years. The artist may become the owner of the land if the artist is in compliance with the state law regarding",
+    answers: [
+      "requirements for a valid conveyance.",
+      "adverse possession.",
+      "avulsion.",
+      "voluntary alienation."
+    ],
+    correctAnswer: "adverse possession.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_13",
+    body: "Eminent domain and escheat are two examples of",
+    answers: [
+      "voluntary alienation.",
+      "adverse possession.",
+      "transfers of title by descent.",
+      "involuntary alienation."
+    ],
+    correctAnswer: "involuntary alienation.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_14",
+    body: "A deed contains a promise that the title conveyed is good and a promise to obtain and deliver any documents necessary to ensure good title. This deed contains an example of which covenant?",
+    answers: [
+      "Further assurances",
+      "Seisin",
+      "Quiet enjoyment",
+      "Warranty forever"
+    ],
+    correctAnswer: "Further assurances",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_15",
+    body: "A deed contains a guarantee that the grantor will compensate the grantee for any loss resulting from the title's failure in the future. This is an example of which covenant?",
+    answers: [
+      "Warranty forever",
+      "Further assurance",
+      "Quiet enjoyment",
+      "Seisin"
+    ],
+    correctAnswer: "Warranty forever",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_16",
+    body: "A person who has died without a will has died",
+    answers: [
+      "testate.",
+      "in valid conveyance.",
+      "intestate.",
+      "under the acknowledgment clause."
+    ],
+    correctAnswer: "intestate.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_17",
+    body: "Title to real estate can be transferred upon death by what type of document?",
+    answers: [
+      "Warranty deed",
+      "Special warranty deed",
+      "Quitclaim deed",
+      "Will"
+    ],
+    correctAnswer: "Will",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_18",
+    body: "An owner of real estate was declared legally incompetent and was committed to a state mental institution. While institutionalized, the owner wrote and executed a will. The owner died while still institutionalized and was survived by a spouse and three children. The real estate will pass",
+    answers: [
+      "to the owner's spouse.",
+      "to the heirs mentioned in the owner's will.",
+      "according to the state laws of descent.",
+      "to the state."
+    ],
+    correctAnswer: "according to the state laws of descent.",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_19",
+    body: "Generally, where does a probate proceeding involving real property take place?",
+    answers: [
+      "Only in the county in which the property is located",
+      "Only in the county in which the decedent resided",
+      "In both the county where the decedent resided and the county in which the property is located",
+      "In the county in which the executor or the beneficiary resides"
+    ],
+    correctAnswer: "In both the county where the decedent resided and the county in which the property is located",
+    math: false,
+    unit: 6
+  },
+  {
+    key: "u6_20",
+    body: "A deed states that the grantors are conveying all their rights and interests to the grantees to have and to hold. This is communicated in",
+    answers: [
+      "the acknowledgment clause.",
+      "the restriction clause.",
+      "the covenant of seisin.",
+      "the habendum clause."
+    ],
+    correctAnswer: "the habendum clause.",
+    math: false,
+    unit: 6
+  }
+];
+
 export const QUESTIONS: Question[] = [
   ...UNIT_1,
   ...UNIT_2,
   ...UNIT_3,
   ...UNIT_4,
-  ...UNIT_5
+  ...UNIT_5,
+  ...UNIT_6
 ];
