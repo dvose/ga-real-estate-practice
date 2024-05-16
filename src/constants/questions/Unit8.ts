b0fb06b912994eeeab863d9ba9bd7c7a7e087456
Dustin Vose <dustin@fivebox.com>
2024-05-16 19:39:10 -0400

    Add Unit 8

--- src/constants/questions/Unit8.ts
+++ src/constants/questions/Unit8.ts
@@ -1,5 +1,265 @@
 import { Question } from "Constants";
 
 const UNIT_8: Question[] = [
+  {
+    key: "u8_1",
+    body: "Which statement BEST explains this sentence: 'To recover a commission for brokerage services, a broker must be employed by a client'?",
+    answers: [
+      "The broker must work in a real estate office.",
+      "The client must make an express or implied agreement to pay a commission to the broker.",
+      "The broker must express an interest in representing the client.",
+      "The broker must have a sales associate employed in the office."
+    ],
+    correctAnswer: "The client must make an express or implied agreement to pay a commission to the broker.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_2",
+    body: "Sales associates who are paid in a lump sum and who are personally responsible for paying their own taxes are probably treated for tax purposes as",
+    answers: [
+      "employees.",
+      "buyer's agents.",
+      "independent contractors.",
+      "transactional brokers."
+    ],
+    correctAnswer: "independent contractors.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_3",
+    body: "A sales associate's contract with her broker states that she is not an employee. In the past year, less than half her income was commission, with the rest an hourly wage paid by the broker. The IRS would classify her as",
+    answers: [
+      "self-employed.",
+      "an employee.",
+      "an independent contractor.",
+      "a part-time real estate salesperson."
+    ],
+    correctAnswer: "an employee.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_4",
+    body: "When acting as an employee rather than an independent contractor, a sales associate may be obligated to",
+    answers: [
+      "list properties in the sales associate's own name.",
+      "work set hours.",
+      "accept a commission from another broker.",
+      "personally pay all withholding taxes."
+    ],
+    correctAnswer: "work set hours.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_5",
+    body: "Terry is the personal assistant of a real estate sales associate. Terry must be paid by the broker who employs the sales associate. This means that",
+    answers: [
+      "Terry has a real estate license.",
+      "the sales associate doesn't make very much money.",
+      "Terry is a part-time employee.",
+      "ultimate responsibility for Terry's conduct in the performance of work-related activities lies with the sales associate."
+    ],
+    correctAnswer: "Terry is a part-time employee.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_6",
+    body: "A broker would have the right to dictate which of the following to an independent contractor?",
+    answers: [
+      "Number of hours the person would have to work",
+      "Work schedule the person would have to follow",
+      "Sales meetings the person would need to attend",
+      "Conduct in compliance with statutory law and regulations"
+    ],
+    correctAnswer: "Conduct in compliance with statutory law and regulations",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_7",
+    body: "Two real estate professionals were found guilty of conspiring with each other to allocate real estate brokerage markets. A seller suffered an estimated $90,000 loss because of their activities. If the seller brings a civil suit against the two real estate professionals, what can the seller expect to recover?",
+    answers: [
+      "Nothing, because a civil suit cannot be brought for damages resulting from antitrust activities",
+      "Only $90,000-the amount of actual damages the seller suffered",
+      "Actual damages plus attorney's fees and costs",
+      "$270,000 plus attorney's fees and costs"
+    ],
+    correctAnswer: "$270,000 plus attorney's fees and costs",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_8",
+    body: "Two sales associates who work for the same firm agree to divide their town into a northern region and a southern region; one sales associate will handle listings in the north, and the other will handle listings in the south. Which statement is TRUE?",
+    answers: [
+      "The agreement does not violate antitrust laws.",
+      "The agreement constitutes illegal price-fixing.",
+      "The two sales associates have violated the Sherman Antitrust Act and are liable for treble damages.",
+      "The two sales associates are guilty of a group boycott with regard to other sales associates in their firm."
+    ],
+    correctAnswer: "The agreement does not violate antitrust laws.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_9",
+    body: "A state has recently updated its Rules and Regulations for the Real Estate Profession. Which statement is most likely TRUE regarding this update?",
+    answers: [
+      "The rules and regulations are state laws enacted by the legislature.",
+      "The rules and regulations do not have the same force and effect as the statutory license law.",
+      "The rules and regulations have the same force and effect as the license law itself.",
+      "The rules and regulations are not enforceable against real estate professionals."
+    ],
+    correctAnswer: "The rules and regulations have the same force and effect as the license law itself.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_10",
+    body: "A REALTOR® is BEST described as an individual who is",
+    answers: [
+      "a real estate professional who acts as a point of contact between two or more people in negotiating the sale, purchase, or rental of property.",
+      "any real estate professional who assists buyers, sellers, landlords, or tenants in any real estate transaction.",
+      "a member of the National Association of Real Estate Brokers who specializes in residential properties.",
+      "a real estate licenses and a member of the National Association of REALTORS® (NAR)."
+    ],
+    correctAnswer: "a real estate licenses and a member of the National Association of REALTORS® (NAR).",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_11",
+    body: "A broker has established the following office policy: 'All listings taken by any sales associate of this real estate brokerage must include compensation based on a 7% commission. No lower commission rate is acceptable.' If the broker attempts to impose this uniform commission requirement, which statement is TRUE.",
+    answers: [
+      "A homeowner may sue the broker for violating the antitrust law's prohibition against price-fixing.",
+      "The sales associates of the brokerage will not be bound by the requirement and may negotiate any commission rate they choose.",
+      "The broker must present the uniform commission policy to the local professional association for approval.",
+      "The broker may, as a matter of office policy, legally set the minimum commission rate acceptable for the firm."
+    ],
+    correctAnswer: "The broker may, as a matter of office policy, legally set the minimum commission rate acceptable for the firm.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_12",
+    body: "A real estate company has adopted a 100% commission plan. The monthly desk rent required of sales associates is $51,500, payable on the last day of the month. In August, a sales associate dosed a transaction that earned a commission of $11,370 and a second transaction that earned a commission of $6,875. The sales associates additional expenses for the month were $2,170. How much of the total monthly income did the sales associate keep?",
+    answers: [
+      "$14,575",
+      "$16,075",
+      "$16,745",
+      "$18,245"
+    ],
+    correctAnswer: "$14,575",
+    math: true,
+    unit: 8
+  },
+  {
+    key: "u8_13",
+    body: "A sales associate took a listing on a house that sold for $329,985. The commission rate was 5%. A sales associate employed by another broker found the buyer. The seller's broker received 60% of the commission on the sale; the buyer's broker received 40%. If the seller's broker kept 30% and paid the seller's sales associate the remainder, how much did the seller's sales associate earn on this sale?",
+    answers: [
+      "$4,346.85",
+      "$5,774.74",
+      "$6,929.68",
+      "$8,249.00"
+    ],
+    correctAnswer: "$6,929.68",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_14",
+    body: "On the sale of any property, a sales associate's compensation is based on the total commission paid to the broker. The sales associate receives 30% of the first $2,500, 40% of the next amount between $2,500 and $7,500, and 50% of any remaining amount exceeding $7,500. If a property sells for $234,500 and the broker's commission rate is 6.5%, what is the sales associate's total compensation?",
+    answers: [
+      "$5,847.00",
+      "$6,621.25",
+      "$6,871.25",
+      "$7,621.25"
+    ],
+    correctAnswer: "$6,871.25",
+    math: true,
+    unit: 8
+  },
+  {
+    key: "u8_15",
+    body: "The federal law that makes contracts originated, negotiated, and executed over a combination of computer and cell phone enforceable is",
+    answers: [
+      "CAN-SPAM.",
+      "Junk Fax Prevention Act.",
+      "COPPA.",
+      "UETA"
+    ],
+    correctAnswer: "UETA",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_16",
+    body: "The amount of commission paid to a sales associate is determined by",
+    answers: [
+      "state law.",
+      "the local real estate board.",
+      "mutual agreement with the broker.",
+      "mutual agreement with the client."
+    ],
+    correctAnswer: "mutual agreement with the broker.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_17",
+    body: "A broker was accused of violating antitrust laws. Of the following, the broker was MOST likely accused of",
+    answers: [
+      "not having an equal housing opportunity sign in the office window.",
+      "undisclosed dual agency.",
+      "price fixing.",
+      "dealing in unlicensed exchange services."
+    ],
+    correctAnswer: "price fixing.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_18",
+    body: "A real estate broker was responsible for a chain of events that resulted in the sale of a client's property. This is called",
+    answers: [
+      "pro forma.",
+      "procuring cause.",
+      "private offering.",
+      "proffered offer."
+    ],
+    correctAnswer: "procuring cause.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_19",
+    body: "A sales associate wants to be classified by the IRS as a qualified real estate agent—the equivalent of holding independent contractor status for tax purposes. The sales associate must meet all of the following requirements EXCEPT",
+    answers: [
+      "receive substantially all income from the brokerage based on production, not time worked.",
+      "perform all work unsupervised by managing broker.",
+      "hold a current real estate license.",
+      "have a written agreement with the broker stating that the sales associate will not be treated as an employee for federal tax purposes."
+    ],
+    correctAnswer: "perform all work unsupervised by managing broker.",
+    math: false,
+    unit: 8
+  },
+  {
+    key: "u8_20",
+    body: "A real estate sales associate, classified by the IRS as an independent contractor, receives",
+    answers: [
+      "a monthly salary or hourly wage.",
+      "company-provided health insurance.",
+      "a company-provided automobile.",
+      "a negotiated share of commissions on transactions."
+    ],
+    correctAnswer: "a negotiated share of commissions on transactions.",
+    math: false,
+    unit: 8
+  }
 ];
 export default UNIT_8;
